67ebc17d6d752b2a764d60744af42267f11756f8
sopstoolGoreleaser <oss@ibotta.com>
2019-01-17 13:46:52 -0700

    Brew formula update for sopstool version v0.3.1

--- Formula/sopstool.rb
+++ Formula/sopstool.rb
@@ -1,9 +1,9 @@
 class Sopstool < Formula
   desc "A multi-file wrapper for Mozilla sops"
   homepage "https://github.com/Ibotta/sopstool"
-  url "https://github.com/Ibotta/sopstool/releases/download/v0.3.0/sopstool_darwin.tar.gz"
-  version "0.3.0"
-  sha256 "c393af1c5cb8697c00ebe2bbb835dc6d0fef534613264f5ab9e271cf1b501ba4"
+  url "https://github.com/Ibotta/sopstool/releases/download/v0.3.1/sopstool_darwin.tar.gz"
+  version "0.3.1"
+  sha256 "a14c7d4bfcfc2e168d70fd852c2e122fa4f887c389ee747b97db7751684773d9"
   
   depends_on "sops"
 
